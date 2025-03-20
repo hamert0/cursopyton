e80e94a8c38995b5259392200db77a1e04172cfa
hamert0 <125414277+hamert0@users.noreply.github.com>
2025-03-19 21:46:30 -0300

    Add files via upload

---
+++ atvaula7.js
@@ -0,0 +1,6 @@
+let nota1= int(input("insira a primeira nota"));
+let nota2= int(input("insira a segunda nota"));
+let nota3= int(imput("insira a terceira nota"));
+let nota4= int(imput("insira a quarta nota"));
+let media= (nota1 + nota2 + nota3 + nota4 / 4);
+console.log(media);
